a114c040ee19c17d0b5cb9af2b2257c77cc2512a
Pierre-Emmanuel Mercier <acesyde@gmail.com>
2021-07-28 22:26:39 +0200

    part3

---
+++ Bullet.gd
@@ -0,0 +1,14 @@
+class_name Bullet
+extends Area2D
+
+export (int) var speed = 10
+
+var direction: Vector2 = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	if direction != Vector2.ZERO:
+		var velocity = direction * speed
+		global_position += velocity
+
+func set_direction(direction: Vector2) -> void:
+	self.direction = direction
--- Player.gd
+++ Player.gd
@@ -1,6 +1,9 @@
 extends KinematicBody2D
 
 export (int) var speed = 100
+export (PackedScene) var Bullet
+
+onready var end_of_gun : Position2D = $EndOfGun
 
 func _physics_process(delta: float) -> void:
 	var movement_direction := Vector2.ZERO
@@ -19,3 +22,15 @@ func _physics_process(delta: float) -> void:
 	move_and_slide(movement_direction * speed)
 	
 	look_at(get_global_mouse_position())
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_released("shoot"):
+		shoot()
+
+func shoot():
+	var bullet_instance: Bullet = Bullet.instance()
+	bullet_instance.global_position = end_of_gun.global_position
+	get_parent().add_child(bullet_instance)
+	var target = get_global_mouse_position()
+	var direction_to_mouse = bullet_instance.global_position.direction_to(target).normalized()
+	bullet_instance.set_direction(direction_to_mouse)
